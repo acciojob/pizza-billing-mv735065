6134f53997c93509d5625ee3e0908481281a32cf
mv735065 <vamshireddy203541@gmail.com>
2024-01-02 21:06:09 +0530

    added boolean

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -1,9 +1,42 @@
 package com.driver;
 
 public class DeluxePizza extends Pizza {
-
+     int s;
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
+
+
         // your code goes here
+        if(isVeg){
+             s=super.getPrice()+150;
+             super.setCheese(true);
+             super.setVegtopplings(true);
+
+
+        }
+        else {
+
+            s=super.getPrice()+200;
+
+        }
+        super.setCheese(true);
+        super.setVegtopplings(true);
+//        this.bill+= "Base Price Of The Pizza: "+this.price+"\n";
+
     }
+
+    public int getPrice(){
+
+        return s;
+    }
+    public String getBill(){
+        // your code goes here
+        String ss=super.getBill();
+
+        return ss;
+    }
+
+
+
+
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,8 +6,26 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private boolean cheese;
+    private  boolean bag;
+
+    private boolean vegtopplings;
+    private boolean nonvegtopplings;
+
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+        if(isVeg){
+           this. price=300;
+        }
+        else {
+            this. price=400;
+
+        }
+        this.bill+= "Base Price Of The Pizza: "+this.price+"\n";
+
+
+
         // your code goes here
     }
 
@@ -17,18 +35,97 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!cheese){
+            this.price+=80;
+            this.bill+="Extra Cheese Added: 80"+"\n";
+            cheese=true;
+        }
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isVeg){
+            if(!vegtopplings){
+                this.price+=70;
+                this.bill+= "Extra Toppings Added: 70"+"\n";
+             vegtopplings=true;
+            }
+
+        }
+        else {
+            if(!nonvegtopplings){
+
+            this.price+=120;
+            nonvegtopplings=true;
+
+
+        this.bill+= "Extra Toppings Added: 120"+"\n";}
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!bag){
+            bag=true;
+            this.bill+="Paperbag Added: 20"+"\n";
+            this.price+=20;
+        }
+
     }
 
     public String getBill(){
         // your code goes here
+        this.bill+="Total Price: " +this.price+ "\n";
+
         return this.bill;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Boolean getVeg() {
+        return isVeg;
+    }
+
+    public void setVeg(Boolean veg) {
+        isVeg = veg;
+    }
+
+    public void setBill(String bill) {
+        this.bill = bill;
+    }
+
+    public boolean isCheese() {
+        return cheese;
+    }
+
+    public void setCheese(boolean cheese) {
+        this.cheese = cheese;
+    }
+
+    public boolean isBag() {
+        return bag;
+    }
+
+    public void setBag(boolean bag) {
+        this.bag = bag;
+    }
+
+    public boolean isVegtopplings() {
+        return vegtopplings;
+    }
+
+    public void setVegtopplings(boolean vegtopplings) {
+        this.vegtopplings = vegtopplings;
+    }
+
+    public boolean isNonvegtopplings() {
+        return nonvegtopplings;
+    }
+
+    public void setNonvegtopplings(boolean nonvegtopplings) {
+        this.nonvegtopplings = nonvegtopplings;
     }
 }
